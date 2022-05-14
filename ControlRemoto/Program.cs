ed4120a8c2436fa44baa4cff15f86b19e526bd30
Christian <christian_ledesma_a@hotmail.com>
2022-05-14 15:06:41 +0200

    Add control project

---
+++ ControlRemoto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace ControlRemoto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comando = "";
+
+            while (true)
+            {
+                comando = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                Console.WriteLine(comando);
+            }
+        }
+    }
+}
+
